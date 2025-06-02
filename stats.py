8e3059291edc92a12a4064c7b2faa697111fb69b
teddysam99 <teddysamyuri19@gmail.com>
2025-06-02 18:41:06 -0400

    Num?:I am so tired, I'm going to shave and eat now.

---
+++ stats.py
@@ -0,0 +1,39 @@
+def count_words(book):
+	book = book.split()
+	num_words = len(book)
+	return(f"{num_words} words found in the document")
+
+
+def lower_case(book):
+	dict = {}
+	lowercasebook = book.lower()
+	for l in lowercasebook:
+		if l not in dict:
+			dict[l] = 1
+		else:
+			dict[l] +=1
+		
+	return(dict)
+
+
+
+def sort_on(dict):
+    return dict["num"] 
+
+def sorted_list(dict):
+	list = []
+	itemss = dict.items()
+
+	for char, num in itemss:
+		if char.isalpha() == True:
+			dict2 = {}
+			dict2["char"] = char
+			dict2["num"] = num
+			list.append(dict2)
+
+	list.sort(reverse=True,key=sort_on)
+
+	return list
+
+
+
